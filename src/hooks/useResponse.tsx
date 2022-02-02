0d035feea10c5fadb84781087fea15b922cad4a8
Markus Tacker <m@coderbyheart.com>
2022-02-03 00:45:31 +0100

    fix: store response

--- src/hooks/useResponse.tsx
+++ src/hooks/useResponse.tsx
@@ -1,4 +1,10 @@
 import { createContext, FunctionComponent, useContext, useState } from 'react'
+import { withLocalStorage } from 'utils/withLocalStorage'
+
+const storedResponse = withLocalStorage<Record<string, any>>({
+	key: 'response',
+	defaultValue: {},
+})
 
 export const ResponseContext = createContext<{
 	response: Record<string, any>
@@ -11,13 +17,16 @@ export const ResponseContext = createContext<{
 export const useResponse = () => useContext(ResponseContext)
 
 export const ResponseProvider: FunctionComponent = ({ children }) => {
-	const [response, update] = useState<Record<string, any>>({})
+	const [response, update] = useState<Record<string, any>>(storedResponse.get())
 
 	return (
 		<ResponseContext.Provider
 			value={{
 				response,
-				update,
+				update: (response) => {
+					update(response)
+					storedResponse.set(response)
+				},
 			}}
 		>
 			{children}
